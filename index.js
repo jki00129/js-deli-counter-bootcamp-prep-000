2c003dc4d60523e1b1f077e7d7b334ebead3d858
Jeewon Kim <jki00129@gmail.com>
2018-07-16 16:57:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,38 +1,10 @@
-function takeANumber(line, name){
-  var katzDeli = [];
-  line.push(name)
-}
+var katzDeliLine = [];
 
-function takeANumber(deliLine,name){
-  var katzDeli = [];
-  deliLine.push(name)
-  
-  return "Welcome, " + name + ". You are number " + deliLine.length + " in line."
-}
-
-function nowServing (deliLine){
-var name;
-if(deliLine.length === 0){
-  return "There is nobody waiting to be served!"
-}
-
-name = deliLine.shift()
-return "Currently serving "+ name + "."
-}
-
-
-function currentLine(deliLine){
-  if(deliLine.length === 0){
-  return "The line is currently empty."
-  } else {
-   var myString = `The line is currently: 1. ${deliLine[0]}`
-
- for(let i = 1; i<deliLine.length;i++){
- myString += `, ${i+1}. ${deliLine[i]}`
-}
-return myString
+function takeANumber(katzDeliLine, name){
+  for(let i = 0; i<katzDeliLine.length; i++){
+    katzDeliLine.push(name)
   }
+  return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`
 }
 
-
-  
+function 
