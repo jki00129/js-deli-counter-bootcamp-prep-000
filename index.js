89eaa0de5516a8e8131f86578c5b55cc74c24174
Jeewon Kim <jki00129@gmail.com>
2018-07-16 16:58:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
+var katzDeliLine = [];
+
+function takeANumber(katzDeliLine, name){
+  for(let i = 0; i<katzDeliLine.length; i++){
+    katzDeliLine.push(name)
+  }
+  return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`
+}
+
+function 
 
